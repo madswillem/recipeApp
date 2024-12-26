099c962f0dfce55a3ed3224a2edf3dec26b5765c
mads <121515803+madswillem@users.noreply.github.com>
2024-12-26 19:02:43 +0100

    added auth and added access controll to the delete route

--- internal/server/userMiddleware.go
+++ internal/server/userMiddleware.go
@@ -1,28 +1,21 @@
 package server
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/madswillem/recipeApp/internal/error_handler"
-	"github.com/madswillem/recipeApp/internal/models"
 )
 
 func (s *Server) UserMiddleware(c *gin.Context) {
-	var user models.UserModel
-	cookie, ok := c.Cookie("user")
-	if ok != nil || cookie == "" {
-		err := user.Create(s.NewDB, c.ClientIP())
-		if err != nil {
-			error_handler.HandleError(c, err.Code, err.Message, err.Errors)
-		}
-		c.SetCookie("user", user.Cookie, 31536000, "/", "localhost", false, true)
-	} else {
-		user.Cookie = cookie
-		err := user.GetByCookie(s.NewDB)
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, err)
-		}
+	tokenString := c.GetHeader("Authorization")
+	if tokenString == "" {
+		error_handler.HandleError(c, 401, "Authorization header required", nil)
+		return
+	}
+
+	err, user := s.Auth.Verify(s.NewDB, tokenString)
+	if err != nil {
+		error_handler.HandleError(c, err.Code, err.Message, err.Errors)
+		return
 	}
 	c.Set("user", user)
 	c.Next()
